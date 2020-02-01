3bef7956b888dcf2a7d9ca47bb2700776bd2bb17
billdaff <billdafeldecker@gmail.com>
2020-02-01 17:53:51 -0500

    update theme.

--- web/themes/custom/billdaff/gulpfile.js
+++ web/themes/custom/billdaff/gulpfile.js
@@ -57,7 +57,7 @@ function js () {
 // Static Server + watching scss/html files
 function serve () {
   browserSync.init({
-    proxy: 'http://yourdomain.com',
+    proxy: 'http://billdaff.ddev.site',
   })
 
   gulp.watch([paths.scss.watch, paths.scss.bootstrap], styles).on('change', browserSync.reload)
